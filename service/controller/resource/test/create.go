36176363cdd1383886ffdae92ae0102f76ce2828
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-12-17 20:17:17 +0200

    Add sample CR parsing

--- service/controller/resource/test/create.go
+++ service/controller/resource/test/create.go
@@ -3,9 +3,18 @@ package test
 import (
 	"context"
 	"fmt"
+
+	"github.com/giantswarm/dns-network-policy-operator/service/key"
+	"github.com/giantswarm/microerror"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
-	fmt.Printf("Object created")
+	cr, err := key.ToDNSNetworkPolicy(obj)
+	if err != nil {
+		return microerror.Mask(err)
+	}
+
+	fmt.Printf("cr %#q with reference networkpolicy %#q", cr.ObjectMeta.Name, cr.Spec.TargetNetworkPolicy)
+
 	return nil
 }
--- service/key/key.go
+++ service/key/key.go
@@ -1,1 +1,21 @@
 package key
+
+import (
+	"github.com/giantswarm/apiextensions/pkg/apis/example/v1alpha1"
+	"github.com/giantswarm/microerror"
+)
+
+func ToDNSNetworkPolicy(v interface{}) (v1alpha1.DNSNetworkPolicy, error) {
+	if v == nil {
+		return v1alpha1.DNSNetworkPolicy{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &v1alpha1.DNSNetworkPolicy{}, v)
+	}
+
+	p, ok := v.(*v1alpha1.DNSNetworkPolicy)
+	if !ok {
+		return v1alpha1.DNSNetworkPolicy{}, microerror.Maskf(wrongTypeError, "expected '%T', got '%T'", &v1alpha1.DNSNetworkPolicy{}, v)
+	}
+
+	c := p.DeepCopy()
+
+	return *c, nil
+}
