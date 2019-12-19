ddbc7a72aa7e3d82e3c23f9b3d076f6650825af3
Volodymyr Stoiko <me@volodymyrstoiko.com>
2019-12-19 13:31:58 +0200

    Fix formatting

--- flag/flag.go
+++ flag/flag.go
@@ -1,8 +1,8 @@
 package flag
 
 import (
-	"github.com/giantswarm/microkit/flag"
 	"github.com/giantswarm/dns-network-policy-operator/flag/service"
+	"github.com/giantswarm/microkit/flag"
 )
 
 // Flag provides data structure for service command line flags.
